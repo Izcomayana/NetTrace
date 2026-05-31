afed4bbb4957628ecc886602835d7140e0c5d40a
izcomayana <oluwapelumisotoyinbo@gmail.com>
2026-05-31 16:38:40 +0100

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
-export default function HomePage() {
-  return <h1>Home</h1>;
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import { StatsBar, CTA, Footer } from "./components/StatsAndCTA";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <StatsBar />
+      <Features />
+      <HowItWorks />
+      <CTA />
+      <Footer />
+    </main>
+  );
 }
-
-
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import Features from "./components/Features";
-// import HowItWorks from "./components/HowItWorks";
-// import { StatsBar, CTA, Footer } from "./components/StatsAndCTA";
-
-// export default function Home() {
-//   return (
-//     <main>
-//       <Navbar />
-//       <Hero />
-//       <StatsBar />
-//       <Features />
-//       <HowItWorks />
-//       <CTA />
-//       <Footer />
-//     </main>
-//   );
-// }
